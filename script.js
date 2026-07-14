d7bf14d1f7642ceed38639f24bdda10aa0c00f42
akemigota-lgtm <akemigota@gmail.com>
2026-07-14 22:43:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,62 @@ function showQuestion(){
     nextBtn.style.display="none";
 
 }
+
+function nextQuestion(){
+
+    if(selectedAnswer === null){
+        return;
+    }
+
+    if(selectedAnswer === questions[currentQuestion].correct){
+        score++;
+    }
+
+    currentQuestion++;
+
+    if(currentQuestion >= questions.length){
+
+        progressBar.style.width = "100%";
+
+        document.getElementById("quiz").style.display = "none";
+        document.getElementById("result").style.display = "block";
+
+        let level = "";
+
+        if(score <= 5){
+            level = "A1 Beginner";
+        }
+        else if(score <= 10){
+            level = "A2 Elementary";
+        }
+        else if(score <= 15){
+            level = "B1 Intermediate";
+        }
+        else if(score <= 20){
+            level = "B2 Upper-Intermediate";
+        }
+        else if(score <= 25){
+            level = "C1 Advanced";
+        }
+        else{
+            level = "C2 Proficient";
+        }
+
+        const percent = Math.round(score / questions.length * 100);
+
+        document.getElementById("score").innerHTML = `
+            <h2>${score} из ${questions.length}</h2>
+
+            <h1 style="color:#3b82f6;margin:20px 0;">
+                ${level}
+            </h1>
+
+            <p>Процент правильных ответов: <b>${percent}%</b></p>
+        `;
+
+        return;
+    }
+
+    showQuestion();
+
+}
